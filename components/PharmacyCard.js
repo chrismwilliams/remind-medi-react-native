49a9cd677cba48e32a7e47efdf8fa6e38b617298
Chris Williams <christophw+github@protonmail.com>
2019-01-13 09:26:50 +0000

    create card for a single pharmacy

--- components/PharmacyCard.js
+++ components/PharmacyCard.js
@@ -1,11 +1,15 @@
 import React, { PureComponent } from "react";
-import { Platform, Text, View, StyleSheet } from "react-native";
+import { Platform, Text, View, Linking, StyleSheet } from "react-native";
 import { WebBrowser } from "expo";
 import { Ionicons } from "@expo/vector-icons";
 
 export default class PharmacyCard extends PureComponent {
   _openBrowser = link => {
     WebBrowser.openBrowserAsync(link);
+  };
+
+  _openTel = num => {
+    Linking.openURL(`tel://${num}`);
   };
 
   render() {
@@ -23,9 +27,12 @@ export default class PharmacyCard extends PureComponent {
           </View>
           <View style={styles.pharmacyDetail}>
             <Ionicons name="ios-cellular" size={26} style={styles.cardIcon} />
-            <Text style={styles.detailText}>
-              {pharmacy.formatted_phone_number}
-            </Text>
+            <TouchableOpacity
+              style={styles.detailText}
+              onPress={() => this._openTel(pharmacy.formatted_phone_number)}
+            >
+              <Text>{pharmacy.formatted_phone_number}</Text>
+            </TouchableOpacity>
           </View>
           {pharmacy.website && (
             <View style={styles.pharmacyDetail}>
