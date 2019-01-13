557ae047ba4430cc9089a3e0a8a68180380b9f21
Chris Williams <christophw+github@protonmail.com>
2019-01-13 09:26:33 +0000

    rm pull to refresh geolocation and methods

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, View, Text } from "react-native";
-import { Text as Heading } from "react-native-elements";
-import { Constants, Location, Permissions } from "expo";
+import { StyleSheet, View } from "react-native";
+import { Text } from "react-native-elements";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 import { GOOGLE_API } from "react-native-dotenv";
@@ -12,51 +11,17 @@ export default class LocationScreen extends Component {
     title: "Pharmacy Search"
   };
   state = {
-    location: null,
-    errorMessage: null,
     pharmacy: null
-  };
-
-  componentWillMount() {
-    if (Platform.OS === "android" && !Constants.isDevice) {
-      this.setState({
-        errorMessage:
-          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
-      });
-    } else {
-      this._getLocationAsync();
-    }
-  }
-
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== "granted") {
-      this.setState({
-        errorMessage: "Permission to access location was denied"
-      });
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
   };
 
   render() {
     let { pharmacy } = this.state;
-    let latitude,
-      longitude = "";
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
-      ({ latitude, longitude } = this.state.location.coords);
-    }
+
     return (
       <View style={styles.container}>
-        <Heading h4 style={styles.textHeading}>
+        <Text h4 style={styles.textHeading}>
           Local Pharmacy Search
-        </Heading>
-        <Text>{latitude}</Text>
-        <Text>{longitude}</Text>
+        </Text>
         <GooglePlacesAutocomplete
           placeholder="Enter a location"
           query={{
