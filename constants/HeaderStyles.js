ad333a84b105a159ce7a7d9f39fb8f7ad394fb61
Chris Williams <christophw+github@protonmail.com>
2019-01-16 09:56:41 +0000

    default header styling

---
+++ constants/HeaderStyles.js
@@ -0,0 +1,11 @@
+import Colors from "./Colors";
+
+export default {
+  headerStyle: {
+    backgroundColor: Colors.primaryColor,
+    borderBottomWidth: 0
+  },
+  headerTitleStyle: {
+    color: "#fff"
+  }
+};
