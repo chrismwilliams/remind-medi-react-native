c3ab9f177b06c47681b5081a207aa309747a45f5
Chris Williams <christophw+github@protonmail.com>
2019-01-31 13:49:15 +0000

    time list now holds time picker

--- components/TimeList.js
+++ components/TimeList.js
@@ -1,45 +1,95 @@
-import React from "react";
+import format from "date-fns/format";
+import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 import { Button, Text } from "react-native-elements";
+import DateTimePicker from "react-native-modal-datetime-picker";
 import Colors from "../constants/Colors";
 
 const formatTime = time => {
   if (!time) return "--:--";
-  const timeDate = new Date(time);
-  const hours = String(timeDate.getHours()).padStart(2, 0);
-  const minutes = String(timeDate.getMinutes()).padStart(2, 0);
-  return `${hours}:${minutes}`;
+  const formattedTime = format(time, "HH:mm:a");
+  return formattedTime;
 };
 
-export default function timeList({
-  numberOfAlerts,
-  currentAlertArray,
-  openTimePicker
-}) {
-  let timesArray = [];
-  for (let i = 0; i < numberOfAlerts; i++) {
-    timesArray.push(
-      <View style={styles.timeWrapper} key={i}>
-        <Button
-          rounded
-          title={`Set time ${i + 1}`}
-          icon={{ name: "access-time", size: 28 }}
-          containerViewStyle={styles.timeButton}
-          backgroundColor="grey"
-          fontWeight="bold"
-          onPress={() => {
-            openTimePicker(i);
-          }}
-        />
-        <View style={styles.chosenTimeWrapper}>
-          <Text style={styles.chosenTime}>
-            {formatTime(currentAlertArray[i])}
-          </Text>
+export default class TimeList extends Component {
+  state = {
+    timePickerVisible: false,
+    timePickPointer: null
+  };
+
+  openTimePicker = index => {
+    this.setState({
+      timePickerVisible: true,
+      timePickPointer: index
+    });
+  };
+
+  handleTimePicked = time => {
+    // call parent with time and pointer
+    this.props.onSubmitTime(this.state.timePickPointer, time);
+
+    this.hideTimePicker();
+  };
+
+  hideTimePicker = () => {
+    this.setState({ timePickerVisible: false, timePickPointer: null });
+  };
+
+  currentTimeSelected = () => {
+    const { currentAlertArray } = this.props;
+    if (!currentAlertArray.length) return new Date();
+
+    const { timePickPointer } = this.state;
+    const timeSelected = currentAlertArray[timePickPointer];
+
+    if (!timeSelected) return new Date();
+    return new Date(timeSelected);
+  };
+
+  renderTimesArray = () => {
+    const { numberOfAlerts, currentAlertArray } = this.props;
+    let timesArray = [];
+    for (let i = 0; i < numberOfAlerts; i++) {
+      timesArray.push(
+        <View style={styles.timeWrapper} key={i}>
+          <Button
+            rounded
+            title={`Set time ${i + 1}`}
+            icon={{ name: "access-time", size: 28 }}
+            containerViewStyle={styles.timeButton}
+            backgroundColor="grey"
+            fontWeight="bold"
+            onPress={() => {
+              this.openTimePicker(i);
+            }}
+          />
+          <View style={styles.chosenTimeWrapper}>
+            <Text style={styles.chosenTime}>
+              {formatTime(currentAlertArray[i])}
+            </Text>
+          </View>
         </View>
+      );
+    }
+    return <View>{timesArray}</View>;
+  };
+
+  render() {
+    const { timePickerVisible } = this.state;
+    return (
+      <View>
+        {this.renderTimesArray()}
+        <DateTimePicker
+          isVisible={timePickerVisible}
+          onConfirm={this.handleTimePicked}
+          onCancel={this.hideTimePicker}
+          mode="time"
+          titleIOS="Pick a time"
+          date={this.currentTimeSelected()}
+        />
       </View>
     );
   }
-  return <View>{timesArray}</View>;
 }
 
 const styles = StyleSheet.create({
@@ -53,7 +103,7 @@ const styles = StyleSheet.create({
     marginLeft: 0
   },
   chosenTimeWrapper: {
-    minWidth: 100,
+    minWidth: 120,
     justifyContent: "center",
     alignItems: "center",
     marginRight: 20,
