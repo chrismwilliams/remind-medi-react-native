daf1f02bfa0d4a541f453fdc8dc3ca11090b23ab
Chris Williams <christophw+github@protonmail.com>
2019-02-01 15:32:55 +0000

    add method to handle form submit

--- screens/ReminderScreen.js
+++ screens/ReminderScreen.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { ScrollView, StyleSheet } from "react-native";
 
-import Colors from "../constants/Colors";
-import StyledTitle from "../components/StyledTitle";
-import HeaderStyles from "../constants/HeaderStyles";
 import ReminderForm from "../components/ReminderForm";
+import StyledTitle from "../components/StyledTitle";
+import Colors from "../constants/Colors";
+import HeaderStyles from "../constants/HeaderStyles";
+import { uniqueID } from "../utils/functions";
 
-export default class ReminderScreen extends React.Component {
+export default class ReminderScreen extends Component {
   static navigationOptions = {
     title: "New Reminder",
     ...HeaderStyles
+  };
+
+  onSubmitForm = alertObj => {
+    console.log(alertObj);
+
+    // period first
+
+    // add unique id
+    const id = uniqueID();
+
+    // save
+
+    // clear form
+
+    // navigate to home screen
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
         <StyledTitle>Add Your Medication</StyledTitle>
-        <ReminderForm />
+        <ReminderForm submitForm={this.onSubmitForm} />
       </ScrollView>
     );
   }
