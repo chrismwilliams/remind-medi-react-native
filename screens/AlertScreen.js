dd1b7ace25069428417c9669e912b80585040696
Chris Williams <christophw+github@protonmail.com>
2019-02-01 15:32:32 +0000

    setup screen to show alert

--- screens/AlertScreen.js
+++ screens/AlertScreen.js
@@ -1,31 +1,54 @@
 import React, { Component } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import { Text } from "react-native-elements";
+
 import StyledTitle from "../components/StyledTitle";
+import AlertCard from "../components/AlertCard";
+
 import Colors from "../constants/Colors";
 import HeaderStyles from "../constants/HeaderStyles";
-import { getReminder } from "../utils/reminder";
+import { getReminder, deleteReminder } from "../utils/reminder";
 
-export default class ReminderScreen extends Component {
+export default class AlertScreen extends Component {
   static navigationOptions = {
     title: "Reminder",
     ...HeaderStyles
   };
 
-  render() {
+  state = {
+    alert: null
+  };
+
+  componentDidMount() {
     const { navigation } = this.props;
-    const alertID = navigation.getParam("id", "no id");
-    const reminder = getReminder(alertID);
+    const alertID = navigation.getParam("id", "");
+    this.setState({ alert: getReminder(alertID) });
+  }
+
+  onPressDelete = () => {
+    const { navigation } = this.props;
+
+    // delete alert
+    deleteReminder(this.state.alert.id);
+
+    // navigate back to home screen
+    navigation.navigate("Home");
+  };
+
+  render() {
+    const { alert } = this.state;
     return (
       <View style={styles.backgroundContainer}>
-        {reminder ? (
+        {alert ? (
           <ScrollView style={styles.container}>
-            <StyledTitle>Your Medication Alert</StyledTitle>
-            <Text>Title: {reminder.title}</Text>
+            <StyledTitle>Your {alert.name} Alert</StyledTitle>
+            <AlertCard alert={alert} onPressDelete={this.onPressDelete} />
           </ScrollView>
         ) : (
           <View style={styles.container}>
-            <Text>Something went wrong 😭</Text>
+            <Text h4 style={styles.errorText}>
+              Something went wrong 😭
+            </Text>
           </View>
         )}
       </View>
@@ -41,5 +64,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 80
+  },
+  errorText: {
+    textAlign: "center",
+    color: "#fff"
   }
 });
