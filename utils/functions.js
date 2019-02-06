c2ff8436a113a06436abf6149068980c3ab83b62
Chris Williams <christophw+github@protonmail.com>
2019-02-06 14:57:49 +0000

    function to sort array of days

--- utils/functions.js
+++ utils/functions.js
@@ -5,3 +5,23 @@ export const uniqueID = () => {
       .substring(2) + new Date().getTime().toString(36)
   );
 };
+
+export const sortDays = daysArray => {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
+  const sortedDaysArray = daysArray.sort((a, b) => {
+    a = days.indexOf(a);
+    b = days.indexOf(b);
+    return a < b ? 0 : 1;
+  });
+
+  return sortedDaysArray;
+};
